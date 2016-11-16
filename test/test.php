e1164a9bdd56043753b34f49484e865b59065935
Richard Hagen <richard.hagen@gmail.com>
2016-11-16 12:38:03 +0400

    fix twig funtion

--- test/test.php
+++ test/test.php
@@ -12,37 +12,7 @@ class Loader implements Twig_LoaderInterface {
 $twig = new Twig_Environment(new Loader());
 
 $renderFunction = new \Richard87\TwigSass\RenderFunction(new \Leafo\ScssPhp\Compiler(),new Richard87\NullCache\NullCachePool());
-//$twig->addExtension($renderFunction);
-
-$twig->addFunction(new Twig_SimpleFunction('renderSass', function (\Twig_Environment $twig,$context, $template) {
-    $compiler = new \Leafo\ScssPhp\Compiler();
-    // Render twig file
-    // TODO: Does TWIG cache this?!
-    $renderedFile = $twig->render($template,$context);
-
-
-    // Render sass
-    $sass = $compiler->compile($renderedFile);
-
-
-    // serve the file
-    return $sass;
-}, array('needs_context' => true, 'needs_environment' => true)));
+$twig->addExtension($renderFunction);
 
 echo $twig->render("index.html.twig",['name' => 'world']);
 
-
-function renderSass(\Twig_Environment $twig,$context, $template) {
-
-    // Render twig file
-    // TODO: Does TWIG cache this?!
-    $renderedFile = $twig->render($template,$context);
-
-
-    // Render sass
-    $sass = $this->compiler->compile($renderedFile);
-
-
-    // serve the file
-    return $sass;
-}
